0a36fab845fdb55e2ef4b0f6492d02d95ba9823f
Daniel Zerlett <d.zerlett@tarent.de>
2018-06-05 14:25:39 +0200

    step 4: static data binding

--- wokshop/src/components/Wok.vue
+++ wokshop/src/components/Wok.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="wok">
-      WOK<br/>
-      ID: {{this.wokID}}
+      {{this.wok.name}}<br/>
+      ID: {{this.wok.id}}<br/>
+      {{this.wok.price}}
+
   </div>
 </template>
 
 <script>
   export default {
     name: 'Wok',
-    props: ['wokID']
+    props: ['wok']
   }
 </script>
 
 <style scoped>
   .wok {
+      padding: 10px;
       color: white;
       background-color: darkgreen;
       font-size: 20px;
       font-weight: bold;
       border: 1px solid black;
-      width: 200px;
-      height: 200px;
+      width: 150px;
+      height: 150px;
       margin-bottom: 10px;
   }
 </style>
--- wokshop/src/components/WokList.vue
+++ wokshop/src/components/WokList.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="wokList">
         <h2>This is the list of available woks:</h2>
-        <Wok wokID="1"/>
-        <Wok wokID="2"/>
-        <Wok wokID="3"/>
+        <Wok v-for="wok in wokList"
+             v-bind:key="wok.id"
+             v-bind:wok="wok"/>
     </div>
 </template>
 
@@ -14,8 +14,17 @@
         name: 'WokList',
         components: {
             Wok
+        },
+        data() {
+            return {
+                wokList: [
+                    {id: 1, name: "Standard Wok", price: "€20.00"},
+                    {id: 2, name: "De Luxe Wok", price: "€30.00"}
+                ]
+            }
         }
     }
+
 </script>
 
 <style scoped>

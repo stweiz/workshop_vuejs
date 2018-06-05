de9c7c05004bea644eb7e0c95c7041c9f4728e8d
Daniel Zerlett <d.zerlett@tarent.de>
2018-06-05 14:11:41 +0200

    step 2: our first component

--- wokshop/src/App.vue
+++ wokshop/src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
     <h1>Welcome to the Wok Shop</h1>
+    <Wok wokID="1" />
+    <Wok wokID="2" />
+    <Wok wokID="3" />
   </div>
 </template>
 
+<script>
+    import Wok from './components/Wok'
+    export default {
+        name: 'App',
+        components: {
+            Wok
+        }
+    }
+</script>

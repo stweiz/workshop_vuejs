93d2cd1f869225e048c76051e746c2502a281cb4
Daniel Zerlett <d.zerlett@tarent.de>
2018-06-15 17:02:30 +0200

    sync script

--- sync_branches.sh
+++ sync_branches.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 branches=()
+
 eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
 for branch in "${branches[@]}"; do
-  if [[ "${branch}" != "master" ]]; then
+  echo $branch
+  if [[ "${branch}" != "refs/heads/master" ]]; then
+    echo processing branch $branch
     git checkout ${branch}
     git checkout master -- README.md
-    git commit -a -m "sync README.md"
+    git commit -m "sync README.md from master"
   fi
 done
 
-git checkout master
+#git checkout master

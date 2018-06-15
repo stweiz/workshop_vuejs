84794d84f336afe871e5b0f394667fa4b4662d0b
Daniel Zerlett <d.zerlett@tarent.de>
2018-06-15 17:07:32 +0200

    sync fix

--- sync_branches.sh
+++ sync_branches.sh
@@ -4,9 +4,10 @@ branches=()
 eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
 for branch in "${branches[@]}"; do
   echo $branch
-  if [[ "${branch}" != "refs/heads/master" ]]; then
-    echo processing branch $branch
-    echo git checkout ${branch}
+  BRANCHSHORT=$(awk -F"/" '{print $3}')
+  if [[ "${BRANCHSHORT}" != "master" ]]; then
+    echo processing branch $BRANCHSHORT
+    echo git checkout $BRANCHSHORT
     echo git checkout master -- README.md
     echo git commit -m "sync README.md from master"
   fi

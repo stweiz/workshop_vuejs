c705943afe282b8f0b3b011dc7e63cacf18c7be7
Stefan Weiz <s.weiz@tarent.de>
2018-11-24 17:12:13 +0100

    Get wok lists by JSON from URL

--- playground/src/components/WokList.vue
+++ playground/src/components/WokList.vue
@@ -2,16 +2,23 @@
     <div>
         <h2>This is the list of available woks:</h2>
         <ul class="wok-list">
-           <li v-for="wok in wokList" v-bind:key="wok.id">
-               <Wok v-bind:wok="wok"/>
-           </li>
+            <li v-for="wok in wokList" v-bind:key="wok.id">
+                <Wok v-bind:wok="wok"/>
+            </li>
         </ul>
         <br style="clear: both"/>
+        <button v-on:click="getWokListStandard">getWokListStandard</button>
+        <br>
+        <button v-on:click="getWokListSpecialCustomer">getWokListSpecialCustomer</button>
     </div>
 </template>
 
 <script>
     import Wok from './Wok'
+    import axios from 'axios'
+
+    const wokJsonUrl = "https://raw.githubusercontent.com/stweiz/workshop_vuejs/master/docs/woks.json";
+    const specialCustomerWokJsonUrl = "https://raw.githubusercontent.com/stweiz/workshop_vuejs/master/docs/woks_special_customer.json";
 
     export default {
         name: 'WokList',
@@ -20,14 +27,26 @@
         },
         data() {
             return {
-                wokList: [
-                    {id: 1, name: "Standard Wok", price: 20.0},
-                    {id: 2, name: "De Luxe Wok", price: 30.0},
-                    {id: 3, name: "Billigwok", price: 10.0},
-                    {id: 4, name: "Wok Besteckset", price: 15.0}
-                ]
+                wokList: {}
             }
-        }
+        },
+        created() {
+            axios.get(wokJsonUrl).then((response) => {
+                this.wokList = response.data["woks"]
+            })
+        },
+        methods: {
+            getWokListStandard: function () {
+                axios.get(wokJsonUrl).then((response) => {
+                    this.wokList = response.data["woks"]
+                })
+            },
+            getWokListSpecialCustomer: function () {
+                axios.get(specialCustomerWokJsonUrl).then((response) => {
+                    this.wokList = response.data["woks"]
+                })
+            }
+        },
     }
 
 </script>
@@ -36,6 +55,7 @@
     ul {
         list-style: none;
     }
+
     li {
         float: left;
         border: 1px solid blue;

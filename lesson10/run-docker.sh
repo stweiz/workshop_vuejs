e9bd5bdde754b6fa38cdc21db98a917bfc4ff474
Stefan Weiz <s.weiz@tarent.de>
2018-12-11 20:31:40 +0100

    Fixes docker run script in lesson10

--- lesson10/run-docker.sh
+++ lesson10/run-docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker run --rm --name wokshop -v dist/:/usr/share/nginx/html nginx:1.15-alpine
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+docker run --rm --name wokshop -p 80:80 -v ${script_dir}/dist:/usr/share/nginx/html nginx:1.15-alpine

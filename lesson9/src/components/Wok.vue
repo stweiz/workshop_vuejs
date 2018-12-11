7abdf3ce275a57c658a0cf0f3558837d4a59ee6b
Stefan Weiz <s.weiz@tarent.de>
2018-12-11 20:31:00 +0100

    Adds lesson9

---
+++ lesson9/src/components/Wok.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <span v-once>{{noChange}}</span>
+        <button v-on:click="changeNotChangeable">Change that</button>
+        <br>
+        <span>{{change}}</span>
+        <button v-on:click="changeChangeable">Change that</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                noChange: "This will never change",
+                change: "This will change",
+            }
+        },
+        methods: {
+            changeChangeable: function () {
+                this.change = "This is changed"
+            },
+            changeNotChangeable: function () {
+                this.noChange = "This can't be changed"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>

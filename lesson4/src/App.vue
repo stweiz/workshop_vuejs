79e30f9ead84c95fa700b55aa2d7577c31ca2d47
Stefan Weiz <s.weiz@tarent.de>
2018-12-11 14:38:55 +0100

    Adds lesson4

--- lesson4/src/App.vue
+++ lesson4/src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <div id="app">
-    <h1>Welcome to the Wok Shop</h1>
-    <WokList/>
-  </div>
+    <div id="app">
+        <h1>Welcome to the Wok Shop</h1>
+        <WokList/>
+    </div>
 </template>
 
 <script>
     import WokList from './components/WokList'
+
     export default {
         name: 'App',
         components: {

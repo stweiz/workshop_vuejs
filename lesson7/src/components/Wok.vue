6de728c9e7d8645948426cc396f778d615c3a5fe
Stefan Weiz <s.weiz@tarent.de>
2018-12-11 20:23:55 +0100

    Fixes message

--- lesson7/src/components/Wok.vue
+++ lesson7/src/components/Wok.vue
@@ -12,7 +12,7 @@
     export default {
         data() {
             return {
-                message: "What's up, doc?",
+                message: "What's up, doc?"
             }
         },
         methods: {

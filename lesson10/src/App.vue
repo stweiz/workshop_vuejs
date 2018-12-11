20e6687b790b7801b5973c67d45d4b941056e2d4
Stefan Weiz <s.weiz@tarent.de>
2018-12-11 20:32:24 +0100

    Adds lesson10

---
+++ lesson10/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <h1>Welcome to the Wok Shop</h1>
+        <WokList/>
+    </div>
+</template>
+
+<script>
+    import WokList from './components/WokList'
+
+    export default {
+        name: 'App',
+        components: {
+            WokList
+        }
+    }
+</script>

4920af414bdaab5e7c04de33b38a8aebfd35ea3f
Stefan Weiz <s.weiz@tarent.de>
2018-12-11 19:58:32 +0100

    Removes lesson5 solution

--- lesson5/src/App.vue
+++ lesson5/src/App.vue
@@ -4,19 +4,16 @@
         <Wok wokId="1" wokName="Basic Wok"/>
         <Wok wokId="2" wokName="Special Wok"/>
         <Wok wokId="3" wokName="Iron Wok"/>
-        <WokSpoon spoonId="1" spoonName="Wooden Spoon"/> <!-- 1 step -->
     </div>
 </template>
 
 <script>
     import Wok from './components/Wok'
-    import WokSpoon from './components/WokSpoon' // 3 step --> Create WokSpoon.vue in components
 
     export default {
         name: 'App',
         components: {
             Wok,
-            WokSpoon //2 step
         }
     }
 </script>
--- lesson5/src/components/WokSpoon.vue
+++ lesson5/src/components/WokSpoon.vue
@@ -1,34 +1,11 @@
 <template>
-    <div class="wokSpoon"> <!-- 5 step -->
-        SPOON
-        ID: {{this.spoonId}} <br/> <!-- 6 step -->
-        Name: {{this.spoonName}} <br/> <!-- 7 step -->
-    </div>
+
 </template>
 
 <script>
-    export default {
-        name: "WokSpoon",
-        props: ["spoonId", "spoonName"] // 8 step
-        /* bonus step
-        props: {
-            spoonId: Number,
-            spoonName: String
-        }
-        */
-    }
+
 </script>
 
 <style scoped>
-    /* 4 step */
-    .wokSpoon {
-        color: white;
-        background-color: darkgreen;
-        font-size: 20px;
-        font-weight: bold;
-        border: 1px solid black;
-        width: 200px;
-        height: 200px;
-        margin-bottom: 10px;
-    }
+
 </style>

27d0f478dd6d0c4ffb711bfe12a893fdb1093bce
Stefan Weiz <s.weiz@tarent.de>
2018-12-11 14:39:00 +0100

    Adds lesson5

--- lesson5/src/App.vue
+++ lesson5/src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="app">
-    <h1>Welcome to the Wok Shop</h1>
-    <Wok wokID="1" />
-    <Wok wokID="2" />
-    <Wok wokID="3" />
-  </div>
+    <div id="app">
+        <h1>Welcome to the Wok Shop</h1>
+        <Wok wokId="1" wokName="Basic Wok"/>
+        <Wok wokId="2" wokName="Special Wok"/>
+        <Wok wokId="3" wokName="Iron Wok"/>
+        <WokSpoon spoonId="1" spoonName="Wooden Spoon"/> <!-- 1 step -->
+    </div>
 </template>
 
 <script>
     import Wok from './components/Wok'
+    import WokSpoon from './components/WokSpoon' // 3 step --> Create WokSpoon.vue in components
+
     export default {
         name: 'App',
         components: {
-            Wok
+            Wok,
+            WokSpoon //2 step
         }
     }
 </script>
--- lesson5/src/components/Wok.vue
+++ lesson5/src/components/Wok.vue
@@ -1,26 +1,27 @@
 <template>
-  <div class="wok">
-      WOK<br/>
-      ID: {{this.wokID}}
-  </div>
+    <div class="wok">
+        WOK<br/>
+        ID: {{this.wokId}}<br>
+        Name: {{this.wokName}}
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'Wok',
-    props: ['wokID']
-  }
+    export default {
+        name: 'Wok',
+        props: ['wokId', "wokName"]
+    }
 </script>
 
 <style scoped>
-  .wok {
-      color: white;
-      background-color: darkgreen;
-      font-size: 20px;
-      font-weight: bold;
-      border: 1px solid black;
-      width: 200px;
-      height: 200px;
-      margin-bottom: 10px;
-  }
+    .wok {
+        color: white;
+        background-color: darkgreen;
+        font-size: 20px;
+        font-weight: bold;
+        border: 1px solid black;
+        width: 200px;
+        height: 200px;
+        margin-bottom: 10px;
+    }
 </style>

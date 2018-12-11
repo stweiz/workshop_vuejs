3444bd1497bb968d7d8cad9f849efb286039e794
Stefan Weiz <s.weiz@tarent.de>
2018-12-11 20:18:00 +0100

    Adds lesson7

---
+++ lesson7/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <h1>Welcome to the Wok Shop</h1>
+        <Wok/>
+    </div>
+</template>
+
+<script>
+    import Wok from './components/Wok'
+
+    export default {
+        name: 'App',
+        components: {
+            Wok
+        }
+    }
+</script>
---
+++ lesson7/src/components/Wok.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input v-model="message">
+        <button v-on:click="reverseMessage">Reverse message in input</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: "What's up, doc?",
+            }
+        },
+        methods: {
+            reverseMessage: function () {
+                this.message = this.message.split('').reverse().join('')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>

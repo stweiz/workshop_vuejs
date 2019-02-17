91991d4559c131027cbadf3ba50f02cf02be4450
Stefan Weiz <s.weiz@tarent.de>
2019-02-17 14:57:59 +0100

    Adds solution for lesson 11 (HTTP requests with modifying data)

--- lesson11/src/components/WokList.vue
+++ lesson11/src/components/WokList.vue
@@ -5,14 +5,14 @@
              v-bind:key="wok.id"
              v-bind:wok="wok"/>
         <!-- TODO Create two buttons, to switch between the data of both JSONs, to show different woks -->
+
+        <button v-on:click="getWokListStandard">Woks</button>
+        <button v-on:click="getWokListSpecialCustomer">Woks for special customer</button>
     </div>
 </template>
 
 <script>
     import Wok from './Wok'
-
-    // (Method 3) If you want to use "axios" in this component only
-    import axios from 'axios'
 
     const wokJsonUrl = "https://raw.githubusercontent.com/stweiz/workshop_vuejs/master/docs/woks.json";
     const specialCustomerWokJsonUrl = "https://raw.githubusercontent.com/stweiz/workshop_vuejs/master/docs/woks_special_customer.json";
@@ -31,19 +31,24 @@
         // So it is evaluated after "computed"
         // The lifecycle hooks are well documented at: https://vuejs.org/v2/api/#Options-Lifecycle-Hooks
         created() {
-            axios.get(wokJsonUrl).then((response) => {
+            this.axios.get(wokJsonUrl).then((response) => {
                 this.wokList = response.data["woks"]
             })
-            /*this.axios.get(wokJsonUrl).then((response) => {
-                this.wokList = response.data["woks"]
-            })*/
-            /*this.$http.get(wokJsonUrl).then((response) => {
-                this.wokList = response.data["woks"]
-            })*/
         },
         methods: {
             // TODO Implement one method, which loads the response of "wokJsonUrl" into wokList
             // and a second method, which loads the response of "specialCustomerWokJsonUrl"
+
+            getWokListStandard: function () {
+                this.axios.get(wokJsonUrl).then((response) => {
+                    this.wokList = response.data["woks"]
+                })
+            },
+            getWokListSpecialCustomer: function () {
+                this.axios.get(specialCustomerWokJsonUrl).then((response) => {
+                    this.wokList = response.data["woks"]
+                })
+            }
         }
     }
 
